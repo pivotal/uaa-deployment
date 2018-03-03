1db88d9e0a9325f9f433643ee38ed2d6b3c219f4
DennisDenuto <dennis.leon39@gmail.com>
2018-03-03 14:50:43 -0800

    Add route for uaa containers via virtualbox vm

--- configure-host.sh
+++ configure-host.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -xeu
+
+sudo route add -net 10.244.0.1/16  192.168.50.6
 UAA_IP=$(bosh -e vbox -d uaa --json --column=ips --column=instance is | jq '.Tables[0].Rows[] | select(.instance | contains("uaa"))' | jq -r .ips)
 sudo echo "$UAA_IP uaa-minimal.bosh-lite.com" >> /etc/hosts
