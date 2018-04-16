99cdded769f533ba55da241f87624117b39ffca1
DennisDenuto <dennis.leon39@gmail.com>
2018-04-15 17:02:41 -0700

    Add oidc and saml host entries to configure-host script

--- configure-host.sh
+++ configure-host.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-set -xeu
+set -xu
 
 sudo route add -net 10.244.0.1/16  192.168.50.6
-UAA_IP=$(bosh -e vbox -d uaa --json --column=ips --column=instance is | jq '.Tables[0].Rows[] | select(.instance | contains("uaa"))' | jq -r .ips)
-sudo echo "$UAA_IP uaa-minimal.bosh-lite.com" >> /etc/hosts
+UAA_IP=$(bosh -e vbox -d uaa --json --column=ips --column=instance is | jq '.Tables[0].Rows[] | select(.instance | contains("uaa/"))' | jq -r .ips)
+sudo echo "$UAA_IP uaa.service.uaa.internal" >> /etc/hosts
+
+SAML_IP=$(bosh -e vbox -d uaa --json --column=ips --column=instance is | jq '.Tables[0].Rows[] | select(.instance | contains("saml/"))' | jq -r .ips)
+sudo echo "$SAML_IP saml.service.uaa.internal" >> /etc/hosts
+
+OIDC_IP=$(bosh -e vbox -d uaa --json --column=ips --column=instance is | jq '.Tables[0].Rows[] | select(.instance | contains("uaa-oidc/"))' | jq -r .ips)
+sudo echo "$OIDC_IP uaa-oidc.service.uaa.internal" >> /etc/hosts
